6ec0965a794da362bdc599c22be110bb3b92f2f8
DaoDuyTuan <tuan123coi96@gmail.com>
2017-08-21 17:36:57 +0700

    committed 17:37 21-08-2017

--- duytuan/Personal.js
+++ duytuan/Personal.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Error from './Error';
 import Table from './Showtable'
 
 class Personal extends React.Component {
@@ -27,71 +26,67 @@ class Personal extends React.Component {
 
     handleUpdate(event) {
         const target = event.target;
-        this.setState({showError: null});
-
         this.setState({
             [target.name]: target.value,
         });
+
+        this.showInputError(target.name);
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        this.setState({});
-        let showError = '';
 
-        for (let item in this.state) {
-            if ((typeof this.state[item] === 'string' && this.state[item].trim() === '') ||
-                (item === 'Age' && isNaN(this.state[item]))) {
+        if (!this.showFormErrors()) {
 
-                showError = <Error errorName={`${item} is empty`}/>;
+            this.setState({showError: "Add fail!"});
+        } else {
 
-                if (item === "Age" && isNaN(this.state[item])) {
-                    showError = <Error errorName={`${item} is not number!!`}/>
-                }
-
-                if (item !== "Gender") {
-                    this.refs[item].focus();
-                }
-
-                // this.deleteError();
-                this.setState({showError: showError});
-                return false;
-            }
-        }
-
-        if (showError === '') {
-            if (this.state.isEdit) {
-                this.personalList.set(this.state.phoneNumber, this.state);
-                //showError = <Error errorName={"Update successful !"}/>;
-                this.addPerson();
-
-                this.setState({showError: showError});
-            } else {
-                if (this.personalList.has(this.state.phoneNumber)) {
-                    showError = <Error errorName={"this person is existed.Please enter again !!!"}/>;
-
-                    this.setState({showError: showError});
-                } else {
-                    this.personalList.set(this.state.phoneNumber, this.state);
-                    //showError = <Error errorName={"Add Successful!!!"}/>;
-                    this.addPerson();
-                    this.setState({showError: showError});
-                }
-            }
+            this.personalList.set(this.state.phoneNumber, this.state);
+            this.addPerson();
         }
     }
 
-    // deleteError(){
-    //     let eleDom = document.getElementById('hideMe').textContent;
-    //     alert(eleDom);
-    //     setTimeout(function(){
-    //         eleDom.style.display = 'none'
-    //     },3000);
-    // }
+    showFormErrors() {
+        const inputs = document.querySelectorAll('input');
+        let isFormValid = true;
+
+        inputs.forEach(input => {
+            const isInputValid = this.showInputError(input.name);
+            const error = document.getElementById(`${input.name}Error`);
+            error.className = "error";
+
+            if (!isInputValid) {
+                isFormValid = false;
+            }
+        });
+
+        return isFormValid;
+    }
+
+    showInputError(refName) {
+        const validity = this.refs[refName].validity;
+        const error = document.getElementById(`${refName}Error`);
+        error.className = "error";
+
+        if (!validity.valid) {
+            if (validity.valueMissing) {
+                error.textContent = `${refName} is a required field`;
+            } else if (validity.typeMismatch) {
+                error.textContent = `${refName} should be a valid email address`;
+            } else if (validity.patternMismatch) {
+                error.textContent = `${refName} should be equal 10 or 11`;
+            }
+
+            return false;
+        }
+
+        error.textContent = '';
+        return true;
+
+    }
+
 
     addPerson() {
-        this.setState({});
-
         let arrPersonal = [];
         this.isShowTable = true;
 
@@ -100,7 +95,10 @@ class Personal extends React.Component {
         });
 
         arrPersonal.sort((a, b) => a.Age - b.Age);
-        this.setState({arrPersonal: arrPersonal});
+        this.setState({
+            arrPersonal: arrPersonal,
+            showError: "Add successful!"
+        });
     }
 
     delPerson(phoneNumber) {
@@ -115,7 +113,6 @@ class Personal extends React.Component {
         this.setState({arrPersonal: arrPersonal});
     }
 
-
     editPerson(phoneNumber) {
         const personal = this.personalList.get(phoneNumber);
         this.setState({
@@ -126,6 +123,7 @@ class Personal extends React.Component {
             workPlace: personal.workPlace,
             phoneNumber: personal.phoneNumber,
             email: personal.email,
+            showError: "Updatting...",
             isEdit: true
         });
     }
@@ -136,23 +134,27 @@ class Personal extends React.Component {
                 <header><h4>I.PERSIONAL INFORMATION</h4></header>
                 <article>
                     {this.state.showError}
-                    <form onSubmit={this.handleSubmit.bind(this)}>
+                    <form>
                         <table>
                             <tbody>
                             <tr>
                                 <td>Full name :</td>
                                 <td>
                                     <input type="text" name="fullName" ref="fullName" value={this.state.fullName}
-                                           onChange={this.handleUpdate.bind(this)}/>
+                                           onChange={this.handleUpdate.bind(this)} required/>
+                                    <div id="fullNameError"/>
                                 </td>
                             </tr>
                             <tr>
                                 <td>Gender:</td>
                                 <td>
-                                    <input type="radio" name="Gender" id="nam" checked={this.state.Gender === 'nam'}
-                                           value="nam" onChange={this.handleUpdate.bind(this)}/> Male
-                                    <input type="radio" name="Gender" id="nu" checked={this.state.Gender === 'nu'}
-                                           value="nu" onChange={this.handleUpdate.bind(this)}/> Female
+                                    <input type="radio" name="Gender" ref="Gender" id="nam"
+                                           checked={this.state.Gender === 'nam'}
+                                           value="nam" onChange={this.handleUpdate.bind(this)} required/> Male
+                                    <input type="radio" name="Gender" ref="Gender" id="nu"
+                                           checked={this.state.Gender === 'nu'}
+                                           value="nu" onChange={this.handleUpdate.bind(this)} required/> Female
+                                    <div id="GenderError"/>
                                 </td>
                             </tr>
 
@@ -160,7 +162,8 @@ class Personal extends React.Component {
                                 <td>Age :</td>
                                 <td>
                                     <input type="text" name="Age" ref="Age" value={this.state.Age}
-                                           onChange={this.handleUpdate.bind(this)}/>
+                                           onChange={this.handleUpdate.bind(this)} required/>
+                                    <div id="AgeError"/>
                                 </td>
                             </tr>
 
@@ -169,7 +172,8 @@ class Personal extends React.Component {
                                 <td>Date of birth :</td>
                                 <td>
                                     <input type="text" name="DOB" ref="DOB" placeholder="mm/dd/yyyy"
-                                           value={this.state.DOB} onChange={this.handleUpdate.bind(this)}/>
+                                           value={this.state.DOB} onChange={this.handleUpdate.bind(this)} required/>
+                                    <div id="DOBError"/>
                                 </td>
 
                             </tr>
@@ -179,7 +183,8 @@ class Personal extends React.Component {
                                 <td>Work place :</td>
                                 <td>
                                     <input type="text" name="workPlace" ref="workPlace" value={this.state.workPlace}
-                                           onChange={this.handleUpdate.bind(this)}/>
+                                           onChange={this.handleUpdate.bind(this)} required/>
+                                    <div id="workPlaceError"/>
                                 </td>
 
                             </tr>
@@ -190,7 +195,9 @@ class Personal extends React.Component {
                                 <td>
                                     <input type="text" name="phoneNumber" ref="phoneNumber"
                                            value={this.state.phoneNumber}
-                                           onChange={this.handleUpdate.bind(this)}/>
+                                           onChange={this.handleUpdate.bind(this)}
+                                           pattern=".{10,11}" required/>
+                                    <div id="phoneNumberError"/>
                                 </td>
 
                             </tr>
@@ -200,14 +207,16 @@ class Personal extends React.Component {
                                 <td>Email :</td>
                                 <td>
                                     <input type="email" name="email" ref="email" placeholder="example@gmail.com"
-                                           value={this.state.email} onChange={this.handleUpdate.bind(this).bind(this)}/>
+                                           value={this.state.email} onChange={this.handleUpdate.bind(this).bind(this)}
+                                           required/>
+                                    <div id="emailError"/>
                                 </td>
 
 
                             </tr>
                             <tr>
                                 <td>
-                                    <input type="submit" value="Submit"/>
+                                    <button onClick={this.handleSubmit.bind(this)}>submit</button>
                                 </td>
                             </tr>
                             </tbody>
@@ -217,7 +226,6 @@ class Personal extends React.Component {
 
                 <br/><br/>
 
-                {console.log(1234)}
                 <Table personal={this.state.arrPersonal} showInfo={this.isShowTable}
                        delPerson={this.delPerson.bind(this)} editPerson={this.editPerson.bind(this)}/>
             </section>
